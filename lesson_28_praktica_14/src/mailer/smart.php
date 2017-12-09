528c521fd871f26a37a9c825330643714d802e27
GeorgeKalyaev <gerakalyaev@mail.ru>
2017-12-09 19:03:56 +0300

    automated_wall_plastering
    automated_wall_plastering

---
+++ lesson_28_praktica_14/src/mailer/smart.php
@@ -0,0 +1,48 @@
+<?php 
+
+$name = $_POST['user_name'];
+$email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+$user_text = $_POST['user_text'];
+
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'jorban.developer@mail.ru';                 // Наш логин
+$mail->Password = 'zxcasdqwezxcasdqwe';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('jorban.developer@mail.ru', 'Письмо от  '.$name['user_name']);   // От кого письмо  //
+$mail->addAddress('kalyaevgera1997@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это тема сообщения';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br>
+	Email: ' . $email . ' <br>
+	Сообщение: ' . $user_text . ' <br>
+	Телефон: ' . $phone . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
